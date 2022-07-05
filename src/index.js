9109f8d755545f2da81b63194e5f0cf77e936018
ducquyet <gunnynhoc7@gmail.com>
2022-07-05 16:38:44 +0700

    update day 1

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const morgan = require('morgan')
+const express = require('express');
+const { engine } = require ('express-handlebars');
+const  path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP Logger
+app.use(morgan('combined'))
+
+//Template engine
+app.engine('hbs', engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('news');
+});
+
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
